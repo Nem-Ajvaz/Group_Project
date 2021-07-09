0d1e7b44cb6c28ae7375480b6ba9baecdfc2a606
Nem-Ajvaz <45509371+Nem-Ajvaz@users.noreply.github.com>
2021-07-09 19:07:46 +1000

    Mergin code

--- asset/js/script.js
+++ asset/js/script.js
@@ -3,27 +3,47 @@ let $yearSearchEle = $("#yearSearch");
 let $containerEle = $("#container");
 
 // Onclick
-$("#search").on("click", function (event) {
+$("#titleSearch").on("keypress", keyPress);
+$("#yearSearch").on("keypress", keyPress);
+$("#search").on("click", searchMovie);
+
+function keyPress(e) {
+  if (e.which == 13) {
+    // enter
+    searchMovie();
+  }
+}
+
+function searchMovie() {
   let $movieTitle = $titleSearchEle.val();
   let $yearMade = $yearSearchEle.val();
 
-  let $fetchURL =
-    "http://www.omdbapi.com/?apikey=986f7dc1&t=" +
-    $movieTitle +
-    "&y=" +
-    $yearMade;
+  let $fetchURL = "http://www.omdbapi.com/?apikey=986f7dc1&t=" + $movieTitle;
+
+  if ($yearMade) $fetchURL += "&y=" + $yearMade;
 
   fetch($fetchURL)
     .then(function (response) {
       return response.json();
     })
-    .then(function (response) {
-      renderResponse();
-    });
-});
-
+    .then(renderResponse);
+}
 let renderResponse = function (response) {
-  console.log("text");
+  // Created Elements for the Movie Title section
+  let $movieTitleEl = $("<h2>");
+  let $movieSummaryTitleEl = $("<h4>");
+  let $movieSummaryEl = $("<p>");
+
+  //Assigning value from FetchURL response
+  $movieTitleEl.text(response.Title);
+  $movieSummaryTitleEl.text("Synopsis");
+  $movieSummaryEl.text(response.Plot);
+
+  //Append elements to container -- Static in HTML
+  $containerEle.empty();
+  $containerEle.append($movieTitleEl);
+  $containerEle.append($movieSummaryTitleEl);
+  $containerEle.append($movieSummaryEl);
 };
 
-renderResponse();
+//srenderResponse();

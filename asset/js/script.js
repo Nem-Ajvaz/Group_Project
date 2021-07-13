12a17e3c426dde8649adc874be21a817141f5a06
Nem-Ajvaz <45509371+Nem-Ajvaz@users.noreply.github.com>
2021-07-13 19:22:43 +1000

    Test

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,36 +1,39 @@
-var searchBtn = $('#search-button');
-var titleinput = $('#movie-input').value;
-var dateinput = $('#date-input').value;
-var titlereturnvalue = "movietitle";// change this with the response title
-var sumreturnvalue = "summary" // change this with the response text
+var searchBtn = $("#search-button");
+var titleinput = $("#movie-input").value;
+var dateinput = $("#date-input").value;
+var titlereturnvalue = "movietitle"; // change this with the response title
+var sumreturnvalue = "summary"; // change this with the response text
 
 //border-solid border-4 border-light-500
 //https://place-hold.it/150
 
-var rendermovieinfo = function(event){
-    event.preventDefault();
+var rendermovieinfo = function (event) {
+  event.preventDefault();
 
-    var title = $('#movie-title');
-    var titleclass = $('#movie-title').parent();
-    titleclass.addClass('flex m-10 h-16 w-1/2 justify-center border-solid border-4 border-light-500' )
-    title.text(titlereturnvalue);
-    $('#movie-content').children('div').addClass('m-10 h-96 w-full lg:w-2/5 border-solid border-4 border-light-500' )
-    $('#summary').text(sumreturnvalue);
-    $('#summary').parent().children('h1').text('summary')
-    var video = $("<img></img>");
-    video.addClass('h-full w-full');
-    video.attr('src','https://place-hold.it/150');
-    video.attr('alt','placeholder');
-    $('#video').append(video)
+  var title = $("#movie-title");
+  var titleclass = $("#movie-title"arent();).p
+  titleclass.addClass(
+    "flex m-10 h-16 w-1/2 justify-center border-solid border-4 border-light-500"
+  );
+  title.text(titlereturnvalue);
+  $("#movie-content")
+    .children("div")
+    .addClass(
+      "m-10 h-96 w-full lg:w-2/5 border-solid border-4 border-light-500"
+    );
+  $("#summary").text(sumreturnvalue);
+  $("#summary").parent().children("h1").text("summary");
+  var video = $("<img></img>");
+  video.addClass("h-full w-full");
+  video.attr("src", "https://place-hold.it/150");
+  video.attr("alt", "placeholder");
+  $("#video").append(video);
 
   console.log(title);
   console.log(titleclass);
+};
 
-}
-
-
-
-searchBtn.on('click',rendermovieinfo);
+// searchBtn.on("click", rendermovieinfo);
 let $titleSearchEle = $("#titleSearch");
 let $yearSearchEle = $("#yearSearch");
 let $containerEle = $("#container");
@@ -77,6 +80,9 @@ let renderResponse = function (response) {
   $containerEle.append($movieTitleEl);
   $containerEle.append($movieSummaryTitleEl);
   $containerEle.append($movieSummaryEl);
+
+  //Adding classes to the created elements
+  $movieTitleEl.addClass("movieTitleHeading");
 };
 
 //srenderResponse();

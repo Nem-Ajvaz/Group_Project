fe139b9818b4e1bba5c38cdde46384ab4e8d3d2f
Nem-Ajvaz <45509371+Nem-Ajvaz@users.noreply.github.com>
2021-07-14 18:27:56 +1000

    Modal works

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,17 +1,20 @@
-var searchBtn = $('#search-button');
-var titleinput = $('#movie-input').value;
-var dateinput = $('#date-input').value;
-var titlereturnvalue = "";// change this with the response title
-var sumreturnvalue = "" // change this with the response text
+const $cancelBtnEle = $("#canelBtn");
+const $titleSearchEle = $("#movie-input");
+const $yearSearchEle = $("#date-input");
+const $containerEle = $("#container");
+const $myModalDisplay = $("#myModalDisplay");
 
-//border-solid border-4 border-light-500
-//https://place-hold.it/150
+console.log($cancelBtnEle.text());
 
-var rendermovieinfo = function (event) {
+$("#canelBtn").on("click", function (event) {
+  location.reload();
+});
+
+let rendermovieinfo = function (event, response) {
   event.preventDefault();
 
-  var title = $("#movie-title");
-  var titleclass = $("#movie-title"arent();).p
+  const title = $("#movie-title");
+  const titleclass = $("#movie-title").parent();
   titleclass.addClass(
     "flex m-10 h-16 w-1/2 justify-center border-solid border-4 border-light-500"
   );
@@ -19,115 +22,64 @@ var rendermovieinfo = function (event) {
   $("#movie-content")
     .children("div")
     .addClass(
-      "m-10 h-96 w-full lg:w-2/5 border-solid border-4 border-light-500"
+      "flex-wrap m-10 h-96 w-full lg:w-2/5 border-solid border-4 border-light-500"
     );
   $("#summary").text(sumreturnvalue);
   $("#summary").parent().children("h1").text("summary");
-  var video = $("<img></img>");
-  video.addClass("h-full w-full");
-  video.attr("src", "https://place-hold.it/150");
-  video.attr("alt", "placeholder");
+  console.log(response.items[0].id.videoId);
+  let video1 =
+    '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/';
+  let video2 =
+    '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+  let video = video1 + response.items[0].id.videoId + video2;
   $("#video").append(video);
-
-  console.log(title);
-  console.log(titleclass);
 };
 
-// searchBtn.on("click", rendermovieinfo);
-let $titleSearchEle = $("#titleSearch");
-let $yearSearchEle = $("#yearSearch");
-let $containerEle = $("#container");
-
 // Onclick
-$("#titleSearch").on("keypress", keyPress);
-$("#yearSearch").on("keypress", keyPress);
-$("#search").on("click", searchMovie);
-
-function keyPress(e) {
-  if (e.which == 13) {
-    // enter
-    searchMovie();
-  }
-}
-
-function searchMovie() {
+$("#search-button").on("click", function (event) {
   let $movieTitle = $titleSearchEle.val();
   let $yearMade = $yearSearchEle.val();
 
-  let $fetchURL = "http://www.omdbapi.com/?apikey=986f7dc1&t=" + $movieTitle;
-
-  if ($yearMade) $fetchURL += "&y=" + $yearMade;
-
-  fetch($fetchURL)
-  .then(function (response) {
-    if (response.ok) {
-      return response.json();
-
-    } else {
-      return Promise.reject(
-        "Failed to retrieve movie information data for: '" + $movieTitle + "'"
-      );
-    }
-  })
-
-    })
-
-    .then(renderResponse);
-
-let renderResponse = function (response) {
-  // Created Elements for the Movie Title section
-  let $movieTitleEl = $("<h2>");
-  let $movieSummaryTitleEl = $("<h4>");
-  let $movieSummaryEl = $("<p>");
-
-  //Assigning value from FetchURL response
-  $movieTitleEl.text(response.Title);
-  $movieSummaryTitleEl.text("Synopsis");
-  $movieSummaryEl.text(response.Plot);
-
-  //Append elements to container -- Static in HTML
-  $containerEle.empty();
-  $containerEle.append($movieTitleEl);
-  $containerEle.append($movieSummaryTitleEl);
-  $containerEle.append($movieSummaryEl);
-
-  //Adding classes to the created elements
-  $movieTitleEl.addClass("moing")
-
-
-    .then(function (movieres) {
-      console.log(movieres)
-      GetYoutube(event, movieres);
-    });
-
-
-let GetYoutube = function (event, movieres) {
-  event.preventDefault();
-  console.log(movieres)
-
-  let $movieTitle = movieres.Search[0].Title;
-  let $yearMade = movieres.Search[0].Year;
-  var API_key = "AIzaSyBhwwEyOaQ-KL-ZGAYniavjGFvgpXsdrpg"
-
   let $fetchURL =
-    "https://www.googleapis.com/youtube/v3/search?part=snippet&key=" +
-    API_key + "&type=video&part=snippet&maxResults=10" + "&q=" + $movieTitle + " " + $yearMade +" trailer";
+    "http://www.omdbapi.com/?apikey=986f7dc1&s=" +
+    $movieTitle +
+    "&y=" +
+    $yearMade;
 
   fetch($fetchURL)
     .then(function (response) {
-      return response.json();
+      if (response.ok) {
+        return response.json();
+      } else (movieres.Error) {
+        $myModalDisplay.removeClass("hidden");
+      }
     })
-    .then(function (response) {
-      rendermovieinfo(event, response);
-    });
+    .then(function (movieres) {
+        console.log(movieres);
+      })
+    
+});
 
-};
+// $("#search-button").on("click", function (event) {
+//   event.preventDefault();
+//   let $movieTitle = $titleSearchEle.val();
+//   let $yearMade = $yearSearchEle.val();
+//   let API_key = "AIzaSyD2eityjzfeKN7WraFzs65wcF8EUBA9tFM";
 
+//   let $fetchURL =
+//     "https://www.googleapis.com/youtube/v3/search?part=snippet&key=" +
+//     API_key +
+//     "&type=video&part=snippet&maxResults=1" +
+//     "&q=" +
+//     $movieTitle +
+//     " " +
+//     $yearMade;
 
-let renderResponse = function (response) {
-  console.log(response);
-
-};
-
-//srenderResponse();
-
+//   fetch($fetchURL)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (response) {
+//       console.log(response);
+//     });
+// });
